d0abb0bdd4db4c4dd95cdeee8ca6562a440cbdf9
Dax Ravi <d@ironbay.co>
2022-08-02 13:45:37 -0400

    More renaming

--- www/sidebars.js
+++ www/sidebars.js
@@ -228,7 +228,7 @@ module.exports = {
         "constructs/KinesisStream"
       ],
       Storage: ["constructs/Bucket"],
-      Auth: ["constructs/Auth"],
+      Auth: ["constructs/Cognito"],
       Types: [
         "constructs/Size",
         "constructs/Duration",
